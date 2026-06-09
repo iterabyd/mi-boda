17ba43db05c69f5d6182383f17cd0037192d93b5
iterabyd <infoiterabyd@gmail.com>
2026-06-09 16:20:13 -0500

    feat:hasta Seccion Nuestra Historia

--- js/main.js
+++ js/main.js
@@ -118,3 +118,57 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+/* SLIDE DE FOTOS */
+const items = document.querySelectorAll(".carousel-item");
+
+let current = 0;
+
+function updateCarousel() {
+
+  items.forEach(item => {
+    item.classList.remove(
+      "active",
+      "prev",
+      "next",
+      "hidden"
+    );
+  });
+
+  const prev =
+    (current - 1 + items.length) %
+    items.length;
+
+  const next =
+    (current + 1) %
+    items.length;
+
+  items[current].classList.add("active");
+  items[prev].classList.add("prev");
+  items[next].classList.add("next");
+
+  items.forEach((item, index) => {
+
+    if (
+      index !== current &&
+      index !== prev &&
+      index !== next
+    ) {
+      item.classList.add("hidden");
+    }
+
+  });
+
+}
+
+updateCarousel();
+
+setInterval(() => {
+
+  current =
+    (current + 1) %
+    items.length;
+
+  updateCarousel();
+
+}, 3500);
